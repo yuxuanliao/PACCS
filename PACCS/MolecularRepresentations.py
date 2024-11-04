b2a84f4fd5c3709fc7b8fa6d20ae7eff3fde31fb
yuxuanliao <103199238+yuxuanliao@users.noreply.github.com>
2024-11-05 01:15:41 +0800

    Construction of molecular representations and model training

---
+++ PACCS/MolecularRepresentations.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  5 00:22:05 2024
+
+@author: yxliao
+"""
+
+import pandas as pd
+import numpy as np
+import torch
+
+
+import rdkit
+from rdkit import Chem
+from rdkit.Chem import Draw
+from rdkit.Chem import AllChem
+from rdkit.Chem import rdMolTransforms
+from rdkit.Chem.rdchem import BondType as BT
+
+from tqdm import tqdm
+from parameters import *
+
+
+def input_data(filename,):
+    data = pd.read_csv(filename)
+    smiles = list(data['SMILES'])
+    adduct = list(data['Adduct'])
+    ccs    = list(data['True CCS'])
+    vpa = list(data['VPA'])
+    mz = list(data['mz'])
+    return smiles, adduct, ccs, vpa, mz
+
+def Standardization(data):
+    data_list = [data[i] for i in data]
+    Max_data, Min_data = np.max(data_list), np.min(data_list)
+    for i in data:
+        data[i] = (data[i] - Min_data) / (Max_data - Min_data)
+    return data
+
+def one_of_k_encoding_unk(x, allowable_set):
+    if x not in allowable_set:
+        x = allowable_set[-1]
+    return list(map(lambda s: x == s, allowable_set))
+
+def GetSmilesAtomSet(smiles):
+    All_Atoms = []
+    for i in range(len(smiles)):
+        mol = Chem.MolFromSmiles(smiles[i])
+        All_Atoms += [atom.GetSymbol() for atom in mol.GetAtoms()]
+        All_Atoms = list(set(All_Atoms))
+    All_Atoms.sort()
+    return All_Atoms
+
+def atom_feature_oneHot(atom, All_Atoms, Atom_radius, Atom_mass):
+    return np.array(
+        # Atomic Type (One-Hot)
+        one_of_k_encoding_unk(atom.GetSymbol() ,All_Atoms) +
+        # Atomic Degree (One-Hot)
+        one_of_k_encoding_unk(atom.GetDegree(), [0, 1, 2, 3, 4]) +
+        # Atomic radius  Atomic mass (float)
+        [Atom_radius[atom.GetSymbol()],Atom_mass[atom.GetSymbol()]] +
+        # Atomic is in Ring ? (One-Hot)
+        one_of_k_encoding_unk(atom.IsInRing(), [0, 1])
+    )
+
+
+def smiles2Graph(smi):
+    m = Chem.MolFromSmiles(smi)
+    mol = Chem.RemoveHs(m)
+    mol3D = Chem.AddHs(m)
+
+    ps = AllChem.ETKDGv3()
+    ps.randomSeed = -1
+    ps.maxAttempts = 1
+    ps.numThreads = 0
+    ps.useRandomCoords = True
+    re = AllChem.EmbedMultipleConfs(mol3D, numConfs = 1, params = ps)
+    re = AllChem.MMFFOptimizeMoleculeConfs(mol3D, numThreads = 0)
+    conf = mol3D.GetConformer()
+
+    N = mol.GetNumAtoms()
+    # 原子特征
+    x = []
+    for atom in mol.GetAtoms():
+        x.append(atom_feature_oneHot(atom, All_Atoms, Atom_radius, Atom_mass))
+    
+    row, col, edge_attr = [], [], []
+    for bond in mol.GetBonds():
+        start, end = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
+        row += [start, end]
+        col += [end, start]
+        
+        # 获取键长
+        bond_length = rdMolTransforms.GetBondLength(conf, start, end)
+        
+        # 将键长与原有的边属性结合
+        edge_attr += 2 * [one_of_k_encoding_unk(bond.GetBondTypeAsDouble(), [1, 1.5, 2, 3]) + [bond_length]]
+        
+    x = torch.tensor(x, dtype=torch.float32)
+    edge_index = torch.tensor([row, col], dtype=torch.long)
+    edge_attr = torch.tensor(edge_attr, dtype=torch.float32)
+    
+    perm = (edge_index[0] * N + edge_index[1]).argsort()
+    edge_index = edge_index[:, perm]
+    edge_attr = edge_attr[perm]
+    
+    return x, edge_attr, edge_index
+
+def load_representations(smiles, adduct_one_hot, ccs, vpa, mz):
+    graph_adduct_data = []
+    Index = 0
+    for smi in tqdm(smiles):
+        
+        g_x, g_e, g_i = smiles2Graph(smi)
+    
+        # label: true CCS
+        y = torch.tensor([ccs[Index]], dtype=torch.float)
+        
+        one_graph = {}
+        one_graph['x'] = g_x
+        one_graph['edge_index'] = g_i
+        one_graph['edge_attr'] = g_e
+        
+        one_graph['y'] = y
+        one_graph['adduct'] = adduct_one_hot[Index]
+        one_graph['vpa'] = torch.tensor([vpa[Index]], dtype=torch.float)
+        one_graph['mz'] = torch.tensor([mz[Index]], dtype=torch.float)
+        graph_adduct_data.append(one_graph)
+        
+        Index += 1
+    return graph_adduct_data
+
+
+
+
+
+
+
+
+
+
+
---
+++ PACCS/Training.py
@@ -0,0 +1,155 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  5 00:48:24 2024
+
+@author: yxliao
+"""
+
+
+import torch
+import torch.nn.functional as F
+from torch.nn import Linear, ReLU, Sequential
+from torch_geometric.data import Data
+from torch_geometric.nn import NNConv
+from MolecularRepresentations import *
+
+
+class PACCS(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        '''Graph 卷积'''
+        self.lin0 = torch.nn.Linear(20, 64)
+        nn = Sequential(Linear(5, 64), ReLU(), Linear(64, 64 * 64))
+        self.conv = NNConv(64, 64, nn, aggr='mean')
+        #self.set2set = Set2Set(dim, processing_steps = 3)  ###
+        self.graph_conv_N = 3
+        
+        '''MLP'''
+        self.bottelneck = torch.nn.Linear(64 + 2 + 3, 384)
+        self.lin1 = torch.nn.Linear(384, 384)
+        self.lin2 = torch.nn.Linear(384, 1)
+        self.MLP_N = 6
+
+    def forward(self, graph, vpa, mz, adduct):
+        out_g = F.relu(self.lin0(graph.x))
+        for i in range(self.graph_conv_N):
+            out_g = F.relu(self.conv(out_g, graph.edge_index, graph.edge_attr))
+            out_g = out_g.squeeze(0)            
+        out_g = torch.sum(out_g, dim=0)
+        
+        out = torch.cat([out_g, vpa, mz, adduct], dim=-1)
+        #out = self.set2set(out)
+        out = F.relu(self.bottelneck(out))
+        for j in range(self.MLP_N):
+            out = F.relu(self.lin1(out))
+        out = self.lin2(out)
+
+        return out
+    
+def PACCS_train(input_path, epochs, batchsize, output_path):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    smiles, adduct, ccs, vpa, mz = read_data(input_path)
+    print('## Read data : ',len(smiles))
+    
+    adduct_SET = list(set(adduct))
+    adduct_SET.sort()
+    print('## Adduct set order : ', adduct_SET)    
+    adduct_one_hot = [(one_of_k_encoding_unk(adduct[_], adduct_SET)) for _ in range(len(adduct))]
+    adduct_one_hot = list(np.array(adduct_one_hot).astype(int))
+    
+    graph_adduct_data = load_representations(smiles, adduct_one_hot, ccs, vpa, mz)
+    split_line1 = int(len(ccs) - len(ccs)*0.2)
+    split_line2 = int(len(ccs) - len(ccs)*0.2) + int(len(ccs) * 0.1)
+    
+    train_adduct_data = graph_adduct_data[:split_line1]
+    valid_adduct_data = graph_adduct_data[split_line1:split_line2]
+    test_adduct_data = graph_adduct_data[split_line2:]
+    
+    
+    print('## The size of the training set : ', len(train_adduct_data))
+    print('## The size of the validation set : ', len(valid_adduct_data))
+
+    train_loader = torch.utils.data.DataLoader(
+        train_adduct_data,
+        shuffle=True,
+        num_workers=0,
+        batch_size=1
+    )
+    
+    valid_loader = torch.utils.data.DataLoader(
+        valid_adduct_data,
+        shuffle=False,
+        num_workers=0,
+        batch_size=1
+    )
+    
+    model = PACCS().to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+    
+    for _ in range(1, epochs+1):
+        batch_loss = 0
+        Index = 0
+        
+        loss_all = []
+        with tqdm(total=len(train_loader)) as p_bar:
+            for data in train_loader:
+                Index += 1
+    
+                graph = Data(
+                    x=data['x'][0], 
+                    edge_index=data['edge_index'][0],
+                    edge_attr=data['edge_attr'][0], 
+                    y=data['y'][0]
+                ).to(device)
+    
+                adduct = data['adduct'][0].to(device)
+                gridpa = data['gridpa'][0].to(device)
+                mw = data['wm'][0].to(device)
+    
+                pred = model(graph, gridpa, mw, adduct)
+    #             loss = F.mse_loss(pred, graph.y)
+                loss = F.huber_loss(pred, graph.y)
+                loss_all.append(loss.cpu().detach().numpy())
+    
+                batch_loss += loss
+                if Index % batchsize == 0:
+                    optimizer.zero_grad()
+                    batch_loss = batch_loss / batchsize
+                    batch_loss.backward()
+                    optimizer.step()
+                    
+                    p_bar.update(batchsize)
+                    p_bar.set_description("Training-Loss {:.2f}".format(batch_loss))
+                    batch_loss = 0
+        train_loss = np.mean(loss_all)
+            
+        loss_all = []
+        with torch.no_grad():
+            for data in tqdm(valid_loader):
+                graph = Data(
+                    x=data['x'][0], 
+                    edge_index=data['edge_index'][0],
+                    edge_attr=data['edge_attr'][0], 
+                    y=data['y'][0]
+                ).to(device)
+            
+                adduct = data['adduct'][0].to(device)
+                gridpa = data['gridpa'][0].to(device)
+                mw = data['wm'][0].to(device)
+    
+                pred = model(graph, gridpa, mw, adduct)
+                
+                # pred = model(graph, line_graph, adduct)
+                # loss = F.mse_loss(pred, graph.y)
+                loss = F.huber_loss(pred, graph.y)
+                loss_all.append(loss.cpu().detach().numpy())
+        val_loss = np.mean(loss_all)
+        # scheduler.step(val_loss)
+        
+        print('train-loss', train_loss, 'val-loss', val_loss)
+
+        torch.save(model.state_dict(), output_path)
+    
+    
