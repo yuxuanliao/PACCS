57c0e9fb3ebb1d5c6cec4775f9e065f4cf01cbe8
yuxuanliao <103199238+yuxuanliao@users.noreply.github.com>
2024-11-16 20:16:29 +0800

    Delete unused packages

--- PACCS/MZ.py
+++ PACCS/MZ.py
@@ -5,12 +5,16 @@ Created on Tue Nov  5 01:29:45 2024
 @author: yxliao
 """
 
-import rdkit
+
 from rdkit import Chem
 from rdkit import RDLogger
 RDLogger.DisableLog('rdApp.*')
 from rdkit.Chem.Descriptors import ExactMolWt
 from tqdm import tqdm
+
+import numpy as np
+import pandas as pd
+from pandas import *
 
 
 def SmilesMW(smiles, adduct):
@@ -29,3 +33,5 @@ def SmilesMW(smiles, adduct):
             cal_mass -= ExactMolWt(Chem.MolFromSmiles('[H+]'))
         MW.append(cal_mass)
     return MW
+
+
