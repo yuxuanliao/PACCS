d29cac134c2787a47b5b3fb1281c148f11448af6
yuxuanliao <103199238+yuxuanliao@users.noreply.github.com>
2025-03-04 22:14:10 +0800

    Modify the code to calculate PA while saving bond lengths

--- PACCS/VoxelProjectedArea.py
+++ PACCS/VoxelProjectedArea.py
@@ -10,11 +10,10 @@ from rdkit import Chem
 from rdkit import RDLogger
 RDLogger.DisableLog('rdApp.*')
 import rdkit.Chem as Chem
-from rdkit.Chem import AllChem
+from rdkit.Chem import AllChem, rdMolTransforms
 import numpy as np
 
 import PACCS.Parameters as parameter
-
 
 
 def fibonacci_sphere(numpts: int):
@@ -60,3 +59,48 @@ def smilesPA(smi, Num=10, cell_radii=0.5, sample_num=100):
         pa = (len(set(p_x)) + len(set(p_y)) + len(set(p_z))) / 3
         PA.append(pa)
     return PA
+
+
+def smilesPA_blengths(smi, Num=10, cell_radii=0.5, sample_num=100):
+    PA = []
+    bond_lengths = None
+    for _ in range(Num):
+        iMol = Chem.MolFromSmiles(smi)
+        iMol3D = Chem.AddHs(iMol)
+    
+        ps = AllChem.ETKDGv3()
+        ps.randomSeed = -1
+        ps.maxAttempts = 1
+        ps.numThreads = 0
+        ps.useRandomCoords = True
+        re = AllChem.EmbedMultipleConfs(iMol3D, numConfs = 1, params = ps)
+        re = AllChem.MMFFOptimizeMoleculeConfs(iMol3D,  numThreads = 0)
+
+        if bond_lengths is None:
+            conf = iMol3D.GetConformer()
+            bond_lengths = []
+            for bond in iMol.GetBonds():
+                start, end = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
+                bond_length = rdMolTransforms.GetBondLength(conf, start, end)
+                bond_lengths.append(bond_length)
+
+        
+        Coords = []
+        pos = fibonacci_sphere(sample_num)
+        
+        for atom in iMol3D.GetAtoms():
+            Symbol = atom.GetSymbol()
+            Coord = list(iMol3D.GetConformer().GetAtomPosition(atom.GetIdx()))
+            Coords.append(np.array(Coord) + pos * parameter.Atomic_radii[Symbol])
+
+        Coords = np.vstack(Coords)
+        grid = (np.array(Coords) // cell_radii).astype(int)
+        grid = np.unique(grid, axis=0)
+
+        p_x = [(i[1],i[2]) for i in grid]
+        p_y = [(i[0],i[2]) for i in grid]
+        p_z = [(i[0],i[1]) for i in grid]
+
+        pa = (len(set(p_x)) + len(set(p_y)) + len(set(p_z))) / 3
+        PA.append(pa)
+    return PA, bond_lengths
