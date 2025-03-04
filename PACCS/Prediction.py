1597ac06c42716e2024f61ec4fcda8f3c097b977
yuxuanliao <103199238+yuxuanliao@users.noreply.github.com>
2025-03-04 22:39:39 +0800

    Update the relevant functions for computational efficiency

--- PACCS/Prediction.py
+++ PACCS/Prediction.py
@@ -181,3 +181,78 @@ def PACCS_predict_woeccs(input_path, model_path, output_path):
     else:
         print("Error: Length mismatch!")
         print(f"Lengths - SMILES: {len(smiles2)}, Adduct: {len(adduct2)}, vpa: {len(vpa2)}, mz: {len(mz2)}, Predicted CCS: {len(pred_ccs)}")
+
+
+def PACCS_predict_blengths(input_path, model_path, output_path):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    smiles, adduct, ccs, vpa, mz, blengths = input_data_blengths(input_path)
+    
+    smiles2 = smiles
+    adduct2 = adduct
+    
+    print('## Read data : ',len(smiles))
+    print('## All Atoms : ', parameter.All_Atoms)
+    print('## All Adduct types: ', parameter.Adduct_SET)
+ 
+    adduct_one_hot = [(one_of_k_encoding_unk(adduct[_], parameter.Adduct_SET)) for _ in range(len(adduct))]
+    adduct_one_hot = list(np.array(adduct_one_hot).astype(int))
+    
+    xmin = parameter.Min_vpa
+    xmax = parameter.Max_vpa
+    for i, x in enumerate(vpa):
+        vpa[i] = (x-xmin) / (xmax-xmin)
+    
+    wmin = parameter.Min_mz
+    wmax = parameter.Max_mz
+    for j, m in enumerate(mz):
+        mz[j] = (m-wmin) / (wmax-wmin)
+    
+    print("Test length :", len(vpa))
+    
+    graph_adduct_data = load_representations_blengths(smiles, adduct_one_hot, ccs, vpa, mz, blengths)
+    
+    test_loader = torch.utils.data.DataLoader(
+        graph_adduct_data,
+        shuffle=False,
+        num_workers=0,
+        batch_size=1
+    )
+    
+    pred_ccs = []
+    true_ccs = []
+    with torch.no_grad():
+        for data in tqdm(test_loader):
+            graph = Data(
+                x=data['x'][0], 
+                edge_index=data['edge_index'][0],
+                edge_attr=data['edge_attr'][0], 
+                y=data['y'][0]
+            ).to(device)
+        
+            adduct = data['adduct'][0].to(device)
+            vpa = data['vpa'][0].to(device)
+            mz = data['mz'][0].to(device)
+    
+            m_state_dict = torch.load(model_path, map_location=torch.device('cpu'))
+            new_m = PACCS().to(device)
+            new_m.load_state_dict(m_state_dict)
+            predict_test = new_m(graph, vpa, mz, adduct)
+            pred_ccs.append(predict_test[0].cpu().numpy().tolist())
+            true_ccs.append(data['y'][0][0].numpy().tolist())
+            
+    if len(smiles2) == len(adduct2) == len(true_ccs) == len(pred_ccs):
+        data2 = {
+            'SMILES': smiles2,
+            'Adduct': adduct2,
+            'True CCS': true_ccs,
+            'Predicted CCS': pred_ccs
+        }
+        df = DataFrame(data2)
+        df.to_csv(output_path, index=False)
+        print('## CCS prediction has been completed')
+    else:
+        print("Error: Length mismatch!")
+        print(f"Lengths - SMILES: {len(smiles2)}, Adduct: {len(adduct2)}, True CCS: {len(true_ccs)}, Predicted CCS: {len(pred_ccs)}")
+
+    re_Metrics = metrics(true_ccs, pred_ccs)
